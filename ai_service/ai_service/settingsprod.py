dfa307803ea89ab74153e0b9ee7b4148cc131e12
Javid <jav.alakbarli@gmail.com>
2025-02-26 01:11:27 +0400

    fixed settingsprod file

--- ai_service/ai_service/settingsprod.py
+++ ai_service/ai_service/settingsprod.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -23,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
